8d1fe76451527ebd32586b2693c460b046cea562
Jasper Lee <bobmugura@gmail.com>
2018-12-28 21:07:07 -0500

    added Materialize and EJS w/ landing page

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
 const port = 8080; // default portnumber
+// configure Express to use EJS
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // default homepage route handler
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    // render the index template
+    res.render("index");
 });
 // start Express server
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "path";
 const app = express();
 const port = 8080; // default portnumber
 
+// configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // default homepage route handler
-app.get("/", (req: any, res: any) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+    // render the index template
+    res.render("index");
 });
 
 // start Express server
